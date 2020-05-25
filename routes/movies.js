a1464376a7da655fb0046ad92b05d1a4b3071df0
Clara Kearney <ckearney@udesa.edu.ar>
2020-05-25 14:11:26 -0300

    había una ruta de más

--- routes/movies.js
+++ routes/movies.js
@@ -2,11 +2,9 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/moviesController');
 
-router.get('/detalle', controller.detail)
-router.get('/', controller.home)
-router.get('/generos', controller.genres)
-router.get('/favoritas', controller.favorite)
-router.get('/home', controller.home)
-
+router.get('/home', controller.home);
+router.get('/detalle', controller.detail);
+router.get('/generos', controller.genres);
+router.get('/favoritas', controller.favorite);
 
 module.exports = router;
