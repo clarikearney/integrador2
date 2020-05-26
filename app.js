5278bce2dab329ab2c2ad45b003a721adbcce2cd
Clara Kearney <ckearney@udesa.edu.ar>
2020-05-26 16:53:07 -0300

    resolvimos localhost

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', moviesRouter);
+app.use('/movies', moviesRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
