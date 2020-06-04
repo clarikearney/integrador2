5bb46ac6addbe90cfcdfb13f8200b893c802a1dd
Clara Kearney <ckearney@udesa.edu.ar>
2020-06-04 17:51:55 -0300

    avance user?

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,128 @@ const bcrypt = require('bcrypt');
 
 let usersController = {
 
+        // getAll: function(req, res) {
+    //     db.User.findAll({
+    //         include:[
+    //             {association: 'Reviews'}
+    //         ] 
+    //     })
+    //     .then(results => {
+    //         let listado = results[0]
+    //         res.render('usersList', {listado: listado})
+    //     })
+    // },
+
+    // REGISTER Y LOG IN
+    // buscar: function(req, res) {
+    //     moduloLogin.chequearUsuario(req.query.email)
+    //     .then(resultado => {
+    //         res.send(resultado)
+    //     })
+    //     },
+    //     register: function(req, res) {
+    //         var salt = bcrypt.genSaltSync(10);
+    //         var hash = bcrypt.hashSync(req.body.password, salt);
+    
+    //         let user = {
+    //             name: req.body.name,
+    //             email: req.body.email,
+    //             password: hash
+    //         }
+    //         db.User.findAll({
+    //             where: [
+    //                 {email: req.body.email}
+    //             ]
+    //         })
+    //         .then(resultado => {
+    //             console.log(req.body.email, resultado == undefined);
+    //             if(resultado == undefined) {
+    //                 return res.render('registerError', {error: 'Email already taken!'})
+    //             } else {
+    //                 db.User.create(user)
+    //                 return res.redirect('/')
+    //             }
+    //         })
+    //     },
+    //     logUser: function(req, res) {
+    //         res.render('login', {tipo: 'log'});
+    //     },
+    //     confirmUser: function(req, res) {
+    //         moduloLogin.validar(req.body.email, req.body.password)
+    //         .then(resultado => {
+    //             if(resultado == undefined) {
+    //                 res.redirect('/users/reviews')
+    //             } else }
+    //             res.redirect('/users/reviews' + resultado.id)
+    //             }
+    //         }
+    //     },
+    
+    //     // SI ESTA OK EL USUARIO MAIL Y CONTRASEÑA, TRAE LAS RESEÑAS DE ESE USUARIO
+    //     getReviews: function(req, res) {
+    //         db.Review.findAll({
+    //             where: [
+    //                 {usuario_id: req.params.id}
+    //             ],
+    //             include: [
+    //                 {association: oneUser}
+    //             ]
+    //         })
+    //         .then(resultado => {
+    //             res.render('reviews', {resultado: resultado})
+    //         })
+    //     },
+    //     showEdit: function(req, res) {
+    //         db.Review.findAll({
+    //             where: [
+    //                 {id: req.params.id}
+    //             ]
+    //         })
+    //         .then(resultado => {
+    //             res.redner('editReview', {resultado: resultado})
+    //         })
+    //     },
+    //     confirmEdit: function(req, res) {
+    //         let updateReview =´
+    //         resena: req.body.resena,
+    //         puntaje: req.body.puntaje,
+    //         id: req.body.id
+    //     }
+    //     db.Review.update({
+    //         resena: updateReview.resena,
+    //         puntaje: updateReview.puntaje
+    //     }, {
+    //         where: {
+    //             id: updateReview.id
+    //         }
+    //     })
+    //     .then(() => {
+    //         db.Resena.findByPk(req.params.id)
+    //         .then(resultado => {
+    //             res.redirect('/users/reviews' + resultado.usuario_id)
+    //         })
+    //     })
+    // },
+    //     deleteReview: function(req, res) {
+    //         res.render('loginForReviewDelete', {deleteId: req.params.id});
+    //     },
+    //     confirmDelete: function (req, res) {
+    //         moduloLogin.validar(req.body.email, req.body.password)
+    //         .then(resultado => {
+    //             if (resultado != null) {
+    //                 db.Review.destroy({
+    //                     where: {
+    //                         id: req.params.id,
+    //                     }
+    //                 })
+    //                 res.redirect('/users/reviews/');
+    //             } else {
+    //                 res.redirect('/users/reviews/delete/' + req.params.id)
+    //             }
+    //         })
+    //     },
+    
+
     searchForm: function (req,res){
         res.render ("searchUser");
     },
